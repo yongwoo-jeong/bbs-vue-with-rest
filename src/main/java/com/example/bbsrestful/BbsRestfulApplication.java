8022df5e8fb15ab7d5a1e14a6ba704ddebfcf764
yongwoojeong <jywoo9@gmail.com>
2023-01-09 19:39:02 +0900

    Init project

---
+++ src/main/java/com/example/bbsrestful/BbsRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.example.bbsrestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbsRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbsRestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bbsrestful/BbsRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bbsrestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbsRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
