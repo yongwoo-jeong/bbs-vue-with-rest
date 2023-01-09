eeda8283281128e84a35197bdf6ba0cae70345f8
yongwoojeong <jywoo9@gmail.com>
2023-01-10 01:36:08 +0900

    Downgrade to vue2 , Installed vuetify

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,13 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  outputDir: '../src/main/resources/static',
-  devServer: {
-    port: 3030,
-    proxy:{
-      '/api': {
-        target: 'http://localhost:8080'
-      }
-    }
-  }
+  transpileDependencies: [
+    'vuetify'
+  ]
 })
