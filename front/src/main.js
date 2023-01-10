2cf26e6afb59fffbda5639dbe6b60cae112d8a6f
yongwoojeong <jywoo9@gmail.com>
2023-01-10 10:25:10 +0900

    Reset to installed vue router

--- front/src/main.js
+++ front/src/main.js
@@ -1,10 +1,5 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify'
+import router from './router'
 
-Vue.config.productionTip = false
-
-new Vue({
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+createApp(App).use(router).mount('#app')
--- front/vue.config.js
+++ front/vue.config.js
@@ -1,6 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ]
-})
+  transpileDependencies: true,
+  outputDir: "../main/resources/static",
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+      },
+    },
+  },
+});
