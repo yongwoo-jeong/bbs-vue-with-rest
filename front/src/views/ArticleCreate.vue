2b7de2b157319c9b85839594ca061c4f12016750
yongwoojeong <jywoo9@gmail.com>
2023-01-23 22:10:28 +0900

    Add styling on ArticleCreate page

--- front/src/views/ArticleCreate.vue
+++ front/src/views/ArticleCreate.vue
@@ -20,25 +20,42 @@
         </tr>
         <tr>
           <th>작성자*</th>
-          <td></td>
+          <td><input name="writer" type="text" /></td>
         </tr>
         <tr>
           <th>비밀번호*</th>
-          <td></td>
+          <td>
+            <input name="password" type="password" placeholder="비밀번호" />
+            <input
+              name="passwordConfirm"
+              type="password"
+              placeholder="비밀번호 확인"
+            />
+          </td>
         </tr>
         <tr>
           <th>제목*</th>
-          <td></td>
+          <td><input name="title" type="text" /></td>
         </tr>
         <tr>
           <th>내용*</th>
-          <td></td>
+          <td class="table-data__content">
+            <input name="content" type="text" />
+          </td>
         </tr>
         <tr>
           <th>파일 첨부</th>
           <td></td>
         </tr>
       </table>
+      <div class="footer__button-containers">
+        <div class="footer__button__cancel">
+          <a>취소</a>
+        </div>
+        <div class="footer__button__submit">
+          <input type="submit" value="저장" />
+        </div>
+      </div>
     </form>
   </main>
 </template>
@@ -75,5 +92,45 @@ td {
 td * {
   width: 300px;
   align-self: baseline;
+}
+
+.table-data__content * {
+  width: 95%;
+  height: 150px;
+  margin: 10px 20px;
+}
+
+.footer__button-containers {
+  width: 1000px;
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.footer__button-containers > div {
+  width: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.footer__button-containers input {
+  padding-left: 30px;
+  padding-right: 30px;
+  background: none;
+  border: none;
+  outline: none;
+}
+
+.footer__button__cancel {
+  background-color: rgba(0, 0, 0, 0.15);
+}
+
+.footer__button__submit {
+  background-color: rgba(0, 0, 0, 0.65);
+}
+
+.footer__button__submit * {
+  color: white;
 }
 </style>
